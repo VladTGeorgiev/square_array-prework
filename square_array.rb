399788c5e0c37250aa653fc4e436e9da001251d5
Vladislav Georgiev <vlad.georgiev@outlook.com>
2019-04-20 08:37:57 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,6 +2,6 @@ def square_array(array)
   sqrt_array = []
   array.each do |element|
     sqrt_number = Math.sqrt(element)
-    #sqrt_array.push(sqrt_number)
+    sqrt_array.push(sqrt_number)
   end
 end
